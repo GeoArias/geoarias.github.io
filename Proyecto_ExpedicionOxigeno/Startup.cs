2b65923812b5ffd8c0d6e2bfc916c9d006e4bf6a
Geovanny Arias <geoas121cr@gmail.com>
2025-05-12 19:34:02 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto_ExpedicionOxigeno/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Proyecto_ExpedicionOxigeno.Startup))]
+namespace Proyecto_ExpedicionOxigeno
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
